d5ac1d3755bd54927eb3e15ce001cddb6bb5b772
Jonathan Munz <jpmunz@gmail.com>
2024-06-20 19:08:52 -0400

    WIP

--- src/reactnativeapp/android/app/src/main/java/com/anonymous/reactnativeapp/MainApplication.kt
+++ src/reactnativeapp/android/app/src/main/java/com/anonymous/reactnativeapp/MainApplication.kt
@@ -2,6 +2,7 @@ package com.anonymous.reactnativeapp
 
 import android.app.Application
 import android.content.res.Configuration
+import android.os.Build
 
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
@@ -11,9 +12,17 @@ import com.facebook.react.ReactHost
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import io.opentelemetry.android.OpenTelemetryRum
+import io.opentelemetry.android.OpenTelemetryRumBuilder
+import io.opentelemetry.android.config.OtelRumConfig
 
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
+import io.opentelemetry.exporter.logging.LoggingSpanExporter;
+import io.opentelemetry.sdk.resources.Resource
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
 
 class MainApplication : Application(), ReactApplication {
 
@@ -45,6 +54,48 @@ class MainApplication : Application(), ReactApplication {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
+
+    val traceURL = when(Build.FINGERPRINT.contains("generic")) {
+      // The Android emulator has a special loopback for localhost
+      true -> "http://10.0.2.2:8080/otlp-http/v1/traces"
+
+        // TODO check for emulator isn't working
+      false -> "http://10.0.2.2:8080/otlp-http/v1/traces"
+    }
+
+    // TODO defer to the resource created by the otel builder, application name isn't being grabbed correctly
+    val resource: Resource =
+      Resource.getDefault().toBuilder().put("service.name", "androidtest").build()
+
+
+    val config = OtelRumConfig()
+    val otelRumBuilder: OpenTelemetryRumBuilder =
+        OpenTelemetryRum.builder(this, config)
+            .addSpanExporterCustomizer {
+                LoggingSpanExporter.create()
+            }
+            /*
+            .addSpanExporterCustomizer {
+                OtlpHttpSpanExporter.builder()
+                    .setEndpoint(traceURL)
+                    .build()
+            }
+
+             */
+    val rum = otelRumBuilder.build()
+
+    val tracer = rum.openTelemetry.getTracer("test")
+    tracer.spanBuilder("my-span-6").startSpan().end();
+
+
+    val client = OkHttpClient()
+
+    val request = Request.Builder()
+      .url("https://example.com/api")
+      .build()
+
+    client.newCall(request).execute()
+
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
   }
 
