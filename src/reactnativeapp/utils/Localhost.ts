16526fe0ec439f4e7b84bcb4dd3478add342c8d0
Jonathan Munz <jpmunz@gmail.com>
2024-06-21 11:10:58 -0400

    fix typo

--- src/reactnativeapp/utils/Localhost.ts
+++ src/reactnativeapp/utils/Localhost.ts
@@ -4,7 +4,7 @@ import {Platform} from 'react-native';
 const getLocalhost = async (): Promise<string> => {
   const isEmulator = await DeviceInfo.isEmulator();
 
-  // The Android emulator has a special lookback for localhost
+  // The Android emulator has a special loopback for localhost
   return Platform.OS === "android" && isEmulator  ?
     "10.0.2.2" : "localhost";
 };
